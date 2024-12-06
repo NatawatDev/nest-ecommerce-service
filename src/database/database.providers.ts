6c3269563bff724be6d93d4c97c0130a61c44ed7
NatawatDev <natwat.codealong@gmail.com>
2024-12-06 10:59:52 +0700

    first commit

--- src/database/database.providers.ts
+++
@@ -1,9 +0,0 @@
-
-import * as mongoose from 'mongoose';
-
-export const databaseProviders = [
-  {
-    provide: 'DATABASE_CONNECTION',
-    useFactory: () => mongoose.connect(process.env.MONGO_URI),
-  },
-];
